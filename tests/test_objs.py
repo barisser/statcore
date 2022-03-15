859ab74bae6d61a902217054ded6fcde2c93ce76
Andrew Barisser <barisser@protonmail.com>
2022-03-15 14:04:24 -0500

    made arithmetic commutative

--- tests/test_objs.py
+++ tests/test_objs.py
@@ -12,4 +12,12 @@ def test_array():
 	assert ar * ar2 == statcore.Array([7, 32, 54])
 	assert ar2 / ar == statcore.Array([7, 2, 1.5])
 	assert ar2 % ar == statcore.Array([0, 0, 3])
-#	import pdb;pdb.set_trace()
+	assert ar2 + 10 == statcore.Array([17, 18, 19])
+	assert ar2 - 5 == statcore.Array([2, 3, 4])
+	assert ar2 * 3 == statcore.Array([21, 24, 27])
+	assert ar2 / 2.0 == statcore.Array([3.5, 4, 4.5])
+	assert ar2 % 3 == statcore.Array([1, 2, 0])
+	assert 30 * ar2 == ar2 * 30
+	assert 1 + ar2 == ar2 + 1
+	assert ar2 / 10 == (1/10) * ar2
+	assert 1 / ar2 == ar2 ** -1
